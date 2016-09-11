e9f5731457e5bfeae12af94db49fd7a8bba62a3a
aminhp93 <minhpn.org.ec@gmail.com>
2016-09-10 17:08:06 -0700

    Login and Registration: validation completetd

--- login_and_registration/login_and_registration.py
+++ login_and_registration/login_and_registration.py
@@ -3,49 +3,67 @@ import re
 from mysqlconnection import MySQLConnector
 
 app = Flask(__name__)
-app.secret_key = "COafsdfsdf"
+app.secret_key = "COafsdfsdfd"
 mysql = MySQLConnector(app, 'amin')
 EMAIL_REGEX = re.compile(r'^[a-zA-Z0-9.+_-]+@[a-zA-Z0-9._-]+\.[a-zA-Z]+$')
 
 
 @app.route('/')
 def index():
+	session['first_name'] = ""
+	session['last_name'] = ""
+	session['email'] = ""
+	session['password'] = ""
+	session['confirm_password'] = ""
 	return render_template('index.html')
 
 @app.route('/submit', methods=['POST'])
 def submit():
 
-	first_name = request.form['first_name']
-	last_name = request.form['last_name']
-	email = request.form['email']
-	password = request.form['password']
-	confirm_password = request.form['password_confirmation']
+	session['first_name'] = request.form['first_name']
+	session['last_name'] = request.form['last_name']
+	session['email'] = request.form['email']
+	session['password'] = request.form['password']
+	session['confirm_password'] = request.form['password_confirmation']
 
-	if len(first_name) < 2 or first_name.isalpha() == False:
-		flash('Name cannot be blank, letter only') 
+	error = 0
+
+	if len(session['first_name']) < 2 or session['first_name'].isalpha() == False:
+		flash('First Name cannot be blank, letter only', 'firstNameError') 
+		error += 1
+
+	if len(session['last_name']) < 2 or session['last_name'].isalpha() == False:
+		flash('Last Name cannot be blank, letter only', 'lastNameError') 
+		error += 1
+
+	if not EMAIL_REGEX.match(session['email']):
+		flash('Invalid Email', 'emailError')
+		error += 1
+
+	if len(session['password']) < 8:
+		flash('Password At least 8 character', 'passwordError')
+		error += 1
+	if session['password'] != session['confirm_password']:
+		flash('Password is not match', 'confirmationPasswordError')
+		error += 1
+
+	if error > 0:
 		return redirect('/')
-	elif len(last_name) < 2 or last_name.isalpha() == False:
-		flash('Name cannot be blank, letter only') 
-		return redirect('/')
-	elif not EMAIL_REGEX.match(email):
-		flash('Invalid Email')
-		return redirect('/')
-	elif len(password) < 8:
-		flash('Password At least 8 character')
-		return redirect('/')
-	elif password != confirm_password:
-		flash('Password is not match')
-		return redirect("/")
 
 	query = "INSERT INTO log_in(first_name, last_name, email, password, created_at, updated_at) VALUES (:first_name, :last_name, :email, :password, NOW(), NOW())"
-	data = {'first_name': first_name, 'last_name': last_name, 'email': email, 'password': password}
+	data = {'first_name': session['first_name'], 'last_name': session['last_name'], 'email': session['email'], 'password': session['password']}
 	session['id'] = mysql.query_db(query, data)
 	return redirect('success')
 
 @app.route('/success')
 def success():
 	query = "SELECT * FROM log_in WHERE id = :id"
-	data = {'id': session['id']}
+
+	if 'id' in session:
+		data = {'id': session['id']}
+	else:
+		data = {'id': session['logged_in_id']}
+	
 	user = mysql.query_db(query, data)
 
 	return render_template('success.html', user = user)
@@ -54,13 +72,24 @@ def success():
 def login():
 	email = request.form['email']
 	password = request.form['password']
-	query = "SELECT * FROM log_in WHERE id = :id"
-	data = {'id': session['id']}
+
+	if 'id' in session:
+		query = "SELECT * FROM log_in WHERE id = :id"
+		data = {'id': session['id']}
+		user = mysql.query_db(query, data)
+		if user[0]['email'] != email or user[0]['password'] != password:
+			flash('Email and Password are not matched')
+			return redirect('/')
+
+	query = "SELECT * FROM log_in WHERE email = :email AND password = :password LIMIT 1"
+	data = {'email': email, 'password': password}
 	user = mysql.query_db(query, data)
 
-	if user[0]['email'] != email or user[0]['password'] != password:
-		flash('Email and Password are not matched')
-		return redirect('/')
+	if len(user) == 0:
+		flash('Wrong password or email when logging in')
+		redirect('/')
+
+	session['logged_in_id'] = user[0]['id']
 
 	return redirect('success')
 app.run(debug=True)
