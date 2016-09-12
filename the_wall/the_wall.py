f9423233f6c3695321ad660daac7438c2d2a87a5
aminhp93 <minhpn.org.ec@gmail.com>
2016-09-12 13:44:57 -0700

    add hashed password using md5 and salt

--- the_wall/the_wall.py
+++ the_wall/the_wall.py
@@ -2,23 +2,21 @@ from flask import Flask, render_template, request, redirect, session, flash
 from mysqlconnection import MySQLConnector
 import re
 import md5
+import os, binascii
 
 app = Flask(__name__)
-app.secret_key = "Cofadsfs"
+app.secret_key = "the wall"
 mysql = MySQLConnector(app, 'the_wall')
 
 EMAIL_REGEX = re.compile(r'^[a-zA-Z0-9.+_-]+@[a-zA-Z0-9._-]+\.[a-zA-Z]+$')
+salt = binascii.b2a_hex(os.urandom(15))
+
+
+# encrypt = md5.new(password + salt).hexdigest();
+
 
 @app.route('/')
 def index():
-    if not 'first_name' in session:
-        session['first_name'] = ""
-    if not 'last_name' in session:
-        session['last_name'] = ""
-    if not 'email' in session:
-        session['email'] = ""
-    if not 'password' in session:
-        session['password'] = ""
     if not 'logged_in_id' in session:
         session['logged_in_id'] = ""
     if not 'id' in session:
@@ -29,39 +27,36 @@ def index():
 
 @app.route('/create', methods=['POST'])
 def create():
-    session['first_name'] = request.form['first_name']
-    session['last_name'] = request.form['last_name']
-    session['email'] = request.form['email']
-    session['password'] = request.form['password']
-    session['password_confirmation'] = request.form['password_confirmation']
+    first_name = request.form['first_name']
+    last_name = request.form['last_name']
+    email = request.form['email']
+    password = md5.new(request.form['password'] + salt).hexdigest();
+    password_confirmation = md5.new(request.form['password_confirmation'] + salt).hexdigest();
 
     # check validation
 
     error = 0
 
-    if len(session['first_name']) < 1:
+    if len(first_name) < 1:
         flash('First name can not be blank', 'firstNameError')
         error += 1
-    elif session['first_name'].isalpha() == False:
-        flash('First name can only contain letter', 'firstNameError')
-        error += 1
 
-    if len(session['last_name']) < 1:
+    if len(last_name) < 1:
         flash('Last name can not be blank', 'lastNameError')
         error += 1
-    elif session['last_name'].isalpha() == False:
-        flash('Last name can only contain letter', 'lastNameError')
-        error += 1
+    # elif last_name.isalpha() == False:
+    #     flash('Last name can only contain letter', 'lastNameError')
+    #     error += 1
 
-    if not EMAIL_REGEX.match(session['email']):
+    if not EMAIL_REGEX.match(email):
         flash('Invalid email', 'emailError')
         error += 1
     
-    if len(session['password']) < 8:
+    if len(password) < 8:
         flash('Password has at least 8 characters', 'passwordError')
         error += 1
 
-    if session['password'] != session['password_confirmation']:
+    if password != password_confirmation:
         flash('Password and confirmation is not match', 'passwordError')
         error += 1
 
@@ -69,14 +64,14 @@ def create():
         return redirect('/')
 
     query = 'INSERT INTO users(first_name, last_name, email, password, created_at, updated_at) VALUES (:first_name, :last_name, :email, :password, NOW(), NOW())'
-    data = {'first_name': session['first_name'], 'last_name': session['last_name'], 'email': session['email'], 'password': session['password']}
+    data = {'first_name': first_name, 'last_name': last_name, 'email': email, 'password': password}
     session['id'] = mysql.query_db(query, data)
     return redirect('/users')
 
 @app.route('/login', methods=['POST'])
 def login():
     email = request.form['email_login']
-    password = request.form['password_login']
+    password = md5.new(request.form['password_login'] + salt).hexdigest();
     
     # check id in session
     if 'id' in session and session['id'] != "":
@@ -105,7 +100,6 @@ def login():
         return redirect('/')
 
     session['logged_in_id'] = user[0]['id']
-    session.items()
 
     return redirect('/users')
 
@@ -155,10 +149,6 @@ def comments(message_id):
 
 @app.route('/logout', methods=['POST'])
 def logout():
-    session.pop('email', None)
-    session.pop('password', None)
-    session.pop('first_name', None)
-    session.pop('last_name', None)
     session.pop('id', None)
     session.pop('logged_in_id', None)
     session.pop('user_id', None)
